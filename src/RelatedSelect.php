daf1ea9b6006040480150a2f5f1a8cb08a4ad960
Molodyko Ruslan <molodyko@samsonos.com>
2016-02-21 15:22:48 +0200

    Added code

---
+++ src/RelatedSelect.php
@@ -0,0 +1,13 @@
+<?php
+namespace samsonphp\js\relatedselect;
+
+use samson\core\CompressableExternalModule;
+
+/**
+ * SamsonPHP related select module.
+ *
+ * @author Ruslan Molodyko <molodyko@samsonos.com>
+ */
+class RelatedSelect extends CompressableExternalModule
+{
+}
